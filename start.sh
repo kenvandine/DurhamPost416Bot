3ea92a323201e009ee06190eb30b9570040aa14c
Ken VanDine <ken@vandine.org>
2025-05-06 23:09:10 -0400

    Fixed if statement

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ set -e
 APP_ID=$(snapctl get app-id)
 BOT_TOKEN=$(snapctl get bot-token)
 
-if [ -n "$APP_ID" ]; then
+if [ -z "$APP_ID" ]; then
     echo "APP_ID required"
     exit 1
 fi
 
-if [ -n "$BOT_TOKEN" ]; then
+if [ -z "$BOT_TOKEN" ]; then
     echo "BOT_TOKEN required"
     exit 1
 fi
